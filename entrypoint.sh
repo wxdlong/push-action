3cc56bccb2e4de033eb0144d0102777e6975cc22
wxdlong <wxdlong@qq.com>
2019-09-16 23:15:12 +0800

    push

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 set -e
 
+ACCESS_TOKEN=$1
+FOLDER=$2
+BRANCH=$3
+CNAME=$4
 if [ -z "$ACCESS_TOKEN" ]
 then
   echo "Please set access_token refer to https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line"
@@ -25,3 +29,12 @@ COMMIT_NAME=wxdlong
 COMMIT_EMAIL=wxdlong@qq.com
 git config --global user.email "${COMMIT_EMAIL}" && \
 git config --global user.name "${COMMIT_NAME}" && \
+
+REMOTE="https://${ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" && \
+
+cd $FOLDER
+git init && \
+git add --all
+git commit -m "Deploy by wxdlong/push-action."
+git remote add origin ${REMOTE}
+git push --force origin master:${BRANCH}
